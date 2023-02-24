4add4338571ed531b633ee1a0a958c3b3aeee366
markseco <mark.anders@hotmail.es>
2023-02-24 23:41:59 +0100

    implemented pomodoro and new layout

--- app.js
+++ app.js
@@ -14,3 +14,5 @@ const songs = [
     audio.src = songs[index];
     audio.play();
   }
+
+
---
+++ pomodoro.js
@@ -0,0 +1,55 @@
+let workTime = 25;
+let breakTime = 5;
+
+let seconds = "00"
+
+// display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    
+}
+
+// start timer
+function start() {
+    
+    // change the time
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    // countdown
+    let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if(workMinutes === -1 ){
+                if(breakCount % 2 === 0) {
+                    // start break
+                    workMinutes = breakMinutes;
+                    breakCount++
+
+                }else {
+                    // continue work
+                    workMinutes = workTime;
+                    breakCount++
+
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 = 1s
+}
