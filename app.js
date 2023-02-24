09f8654eb609ac5650faf806162bbdb43c98c72c
markseco <mark.anders@hotmail.es>
2023-02-24 19:21:25 +0100

    Added game, centered many things and applied new fonts

---
+++ app.js
@@ -0,0 +1,16 @@
+const songs = [
+    "chill.mp3",
+    "chill.mp3",
+    "chill.mp3",
+    // add more songs as needed
+  ];
+  
+
+  function next() {
+
+    const index = Math.floor(Math.random() * songs.length);
+    const audio = document.getElementById("audio");
+    
+    audio.src = songs[index];
+    audio.play();
+  }
