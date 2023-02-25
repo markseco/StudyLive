d82ccd1247726fde0f69efba045f3ca0a1ca8709
markseco <mark.anders@hotmail.es>
2023-02-25 19:38:37 +0100

    add pause button

--- pomodoro.js
+++ pomodoro.js
@@ -3,19 +3,18 @@ let breakTime = 5;
 
 let intervalId;
 
-let seconds = "00"
+let seconds = "00";
+let isPaused = false; // new variable
 
 // display
 window.onload = () => {
     document.getElementById('minutes').innerHTML = workTime;
     document.getElementById('seconds').innerHTML = seconds;
-
-    
+    document.getElementById('pause').addEventListener('click', pause); // new line
 }
 
 // start timer
 function start() {
-    
     clearInterval(intervalId);
 
     // change the time
@@ -28,32 +27,39 @@ function start() {
 
     // countdown
     let timerFunction = () => {
-        //change the display
-        document.getElementById('minutes').innerHTML = minutes;
-        document.getElementById('seconds').innerHTML = seconds;
+        if (!isPaused) { // new line
+            //change the display
+            document.getElementById('minutes').innerHTML = minutes;
+            document.getElementById('seconds').innerHTML = seconds;
 
-        // start
-        seconds = seconds - 1;
+            // start
+            seconds = seconds - 1;
 
-        if(seconds === 0) {
-            minutes = minutes - 1;
-            if(minutes === -1 ){
-                if(breakCount % 2 === 0) {
-                    // start break
-                    minutes = minutesBreak;
-                    breakCount++
+            if(seconds === 0) {
+                minutes = minutes - 1;
+                if(minutes === -1 ){
+                    if(breakCount % 2 === 0) {
+                        // start break
+                        minutes = minutesBreak;
+                        breakCount++
 
-                }else {
-                    // continue work
-                    minutes = workTime;
-                    breakCount++
+                    }else {
+                        // continue work
+                        minutes = workTime;
+                        breakCount++
 
+                    }
                 }
+                seconds = 59;
             }
-            seconds = 59;
         }
     }
 
     // start countdown
     intervalId = setInterval(timerFunction, 1000); // 1000 = 1s
+}
+
+// pause timer
+function pause() {
+    isPaused = !isPaused;
 }
